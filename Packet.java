88e94937cc0ac2d07fc6e993f48f9194b9169f68
r11sr <rs_burner2@outlook.com>
2022-10-14 20:10:23 -0500

    completed documentation of packet class

--- Packet.java
+++ Packet.java
@@ -1,16 +1,20 @@
 
 /**
- * Write a description of class Packet here.
+ * Thread 3 takes capitalised characters and add them to packets.
+ * the packet then formats the characters and when printing
+ * displays the amount of packets generated upon till and including the 
+ * current, the amount of characters in the current packet and the characters 
+ * themselves.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Rasheed R. Senior
+ * @version 1.0.0
  */
 public class Packet
 {
-    private final int MAX_PACKET_SIZE = 5;
+    private final int MAX_CHAR_IN_PACKET_SIZE = 5;
     private static int packet_count = -1;
-    private int charCount = 0;
-    char content[] = new char[MAX_PACKET_SIZE];
+    private int charCount = 0; // records the amount of characters currently in the packet
+    char content[] = new char[MAX_CHAR_IN_PACKET_SIZE]; // array of char to store the contents of the packet
 
     /**
      * Constructor for objects of class Packet
@@ -20,22 +24,44 @@ public class Packet
         packet_count++;
     }
 
-
+    /**
+    * This method adds a captialised buffer to the packet
+    *        
+    * @param ch  capatilized letter to add to the packet
+     */
     public void Add(char ch){
         content[charCount] = ch;
         charCount++;
     }
     
+    /**
+    * Inquires to see if the buffer is full or not
+    *        
+    * @return state of the buffer, full or not
+     */
+    
     public boolean full(){
     
-        if(charCount >= MAX_PACKET_SIZE){return true;}
+        if(charCount >= MAX_CHAR_IN_PACKET_SIZE){return true;}
         return false;
     }
+    
+    /**
+    * Inquries of the amount of items in the buffer
+    *        
+    * @return amount of characters in the packet
+     */
     
     public int quantity(){
     return charCount;
     }
     
+    
+    /**
+    * This method returns the packet in the desired human readable format
+    *        
+    * @return the packet as s string
+     */
     public String toString(){
         StringBuilder packetInfo = new StringBuilder("");
         
